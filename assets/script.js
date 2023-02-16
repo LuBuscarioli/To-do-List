0d19dac830a15cf34d401574aaa367fd078201a0
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2023-02-15 22:59:36 -0300

    update tasks

--- assets/script.js
+++ assets/script.js
@@ -62,19 +62,19 @@ const disableButtons = (bool) => {
     });
 };
 
-//remove task from local storage 
+//Remove task from local storage 
 const removeTask = (taskValue) => {
     localStorage.removeItem(taskValue);
     displayTasks();
 }
 
-//add tasks to local storage
+//Add tasks to local storage
 const updateStorage = (index, taskValue, completed) => {
     localStorage.setItem(`${index}_${taskValue}`, completed);
     displayTasks();
 };
 
-//function to add new task 
+//Function to add new task 
 document.querySelector("#push").addEventListener("click", () => {
     //Enable edit button
     disableButtons(false);
